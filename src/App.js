8a7dfb2009a065d3451dfd6d8b3ad3336068b278
jason1110 <Jason.easterly11@gmail.com>
2020-12-03 10:23:29 -0700

    add a new todo form and a delete butotn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import  { Component } from 'react' 
 import './App.css';
 import TodoContainer from './components/TodoContainer';
+import TodoForm from './components/TodoForm'
 
 const todosURL = 'http://localhost:3000/todos'
 
@@ -21,11 +22,37 @@ class App extends Component {
     .then(todos => this.setState( {todos} ))
   }
 
+  addTodo = (newTodo) => {
+    this.setState ({
+      todos: [...this.state.todos, newTodo]
+    })
+
+    fetch(todosURL, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json" 
+      },
+      body: JSON.stringify(newTodo)
+    })
+  }
+
+  deleteTodo = (id) => {
+    let filterdTodos = this.state.todos.filter( todo => todo.id !== id )
+    this.setState({
+      todos: filterdTodos
+    })
+    fetch(todosURL + '/' + id, {
+      method: 'DELETE',
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
         <h1>Todo App</h1>
-        <TodoContainer  todos={this.state.todos} />
+        <TodoForm addTodo={this.addTodo} />
+        <TodoContainer  todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-export default function TodoItem({title, content}) {
+export default function TodoItem({title, content, deleteTodo, id}) {
+    
+    const handleClick = (event) => deleteTodo(id)
+    
     return (
         <div>
             <li className='todo-item'>
                 <h2>{title}</h2>
                 <h3>{content}</h3>
+                <button onClick={handleClick} className='delete-button'>DELETE</button>
             </li>
         </div>
     )
